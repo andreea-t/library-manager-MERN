5e49096856bd8e277fbbd319dfe860385d42989d
Andreea <andreea.trufan@gmail.com>
2020-11-21 17:37:57 +0200

    Project structure

---
+++ library-manager/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+          {/* Nav */}
+            {/* Book list */}
+                {/* GET */}
+
+            {/* Add book */}
+                {/* Form with POST */}
+
+            {/* Edit book */}
+                {/* Form with UPDATE and DELETE */}
+
+            {/* Add user */}
+                {/* Form with POST */}
+
+            {/* Edit user */}
+                {/* Form with UPDATE and DELETE */}
+    </div>
+  );
+}
+
+export default App;
